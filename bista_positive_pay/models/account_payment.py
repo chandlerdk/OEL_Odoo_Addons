0ac9d9fc0e9ed4fd784fcb6f15154b8c149f1ada
bistabrandon <110696162+bistabrandon@users.noreply.github.com>
2024-09-05 10:27:53 -0600

    Add Positive Pay

---
+++ bista_positive_pay/models/account_payment.py
@@ -0,0 +1,50 @@
+import base64
+
+from odoo import models
+from odoo.exceptions import UserError
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    def report_positive_pay(self):
+        file_content = "\n".join(payment.get_line() for payment in self)
+        file_data = base64.b64encode(file_content.encode('utf-8'))
+        attachment = self.env['ir.attachment'].create({
+            'name': 'Positive Pay.txt',
+            'type': 'binary',
+            'datas': file_data,
+            'store_fname': 'positive_pay.txt',
+            'mimetype': 'text/plain',
+        })
+        return {
+            'type': 'ir.actions.act_url',
+            'url': '/web/content/%s?download=true' % (attachment.id),
+            'target': 'new',
+        }
+
+    def get_line(self):
+        account_number = self.journal_id.default_account_id.code
+        check = self.check_number
+        date = self.date.strftime("%m%d%y")
+        amount = int(self.amount)
+        partner = self.partner_id.name
+        check_type = "I" if self.state == 'posted' else "V"
+        check_number = check + str(date)
+
+        reserve_1_4 = "    "
+        available_characters = 22 - (len(account_number) + len(check_number))
+        if available_characters < 0:
+            if len(check_number) > 15:
+                raise UserError("Check number is exceeding the 9 digit limit")
+            if len(account_number) > 17:
+                raise UserError("Account number is exceeding the 17 digit limit")
+        if len(str(amount)) > 9:
+            raise UserError("Amount is exceeding the 9 digit limit")
+
+        reserve_27_29 = "  "
+        amount_holder = str(amount).ljust(9, "0")[:9]
+        reserve_28 = " "
+        return reserve_1_4 + account_number.ljust(available_characters, "0")[
+                             :17] + check_number + reserve_27_29 + amount_holder + reserve_28 + check_type + partner[
+                                                                                                             :128]
