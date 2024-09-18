62aa428a953a48a41d4c52bd4507f0dc3f8b0e62
Punit Chaudhary <punit.chaudhary@bista-it-00220.bistasolutions.com>
2024-09-18 22:07:12 +0530

    [ADD]Remove shipsttaion module

--- bista_shipstation/models/stock_package_type.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ProductPackaging(models.Model):
-    _inherit = 'stock.package.type'
-
-    height = fields.Float(string="Height")
-    width = fields.Float(string="Width")
-    packaging_length = fields.Float(string="Length")
