2b0ae6173c85038044fbb72b23a7169c9e2f4381
Punit Chaudhary <punit.chaudhary@bista-it-00220.bistasolutions.com>
2024-09-18 17:31:57 +0530

    [ADD]Imp in code for as per the update

--- bista_completed_saleorder/reports/sale_report.py
+++ bista_completed_saleorder/reports/sale_report.py
@@ -4,9 +4,9 @@ from odoo import fields, models
 class SaleReport(models.Model):
     _inherit = 'sale.report'
 
-    complete_order = fields.Boolean('Complete Orders')
+    flag = fields.Boolean('Margin')
 
     def _select_additional_fields(self):
         res = super()._select_additional_fields()
-        res['complete_order'] = "s.complete_order"
+        res['flag'] = "s.flag"
         return res
