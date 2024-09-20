f048543e024a7f01db3f80474043fffffc7a2b56
Punit Chaudhary <punit.chaudhary@bista-it-00220.bistasolutions.com>
2024-09-20 21:24:26 +0530

    [ADD]Imp in code for complete order

--- bista_completed_saleorder/models/sale.py
+++ bista_completed_saleorder/models/sale.py
@@ -5,18 +5,20 @@ from odoo import api, models, fields, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    complete_order = fields.Boolean(string="Completed Orders", compute='compute_order_line')
+    complete_order = fields.Boolean(string="Completed Orders",compute='compute_order')
     flag = fields.Boolean(default=False)
 
-    def compute_order_line(self):
-        if self.order_line:
-            for rec in self.order_line:
-                if rec.product_uom_qty == rec.qty_delivered == rec.qty_invoiced:
-                    self.complete_order = True
-                    self.flag = True
-                else:
-                    self.complete_order = False
-                    self.flag = False
+    @api.depends('order_line.product_uom_qty','order_line.qty_delivered','order_line.qty_invoiced')
+    def compute_order(self):
+        for rec in self:
+            if all([order.product_uom_qty == order.qty_delivered == order.qty_invoiced for order in rec.order_line.filtered(lambda p: p.product_id.detailed_type != 'service')]) == True:
+                rec.flag = True
+                rec.complete_order = True
+            else:
+                rec.flag = False
+                rec.complete_order = False
+
+
 
 
 
