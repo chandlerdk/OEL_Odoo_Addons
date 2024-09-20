fe4a2d1cbb6d44aa75507c050d1187dd061f704f
Sachin Shelke <sachin.shelke@bista-it-00274.bistasolutions.com>
2024-09-20 16:52:48 +0530

    [ADD] purchase res config setting

--- purchase_order_fob_inhancement/models/res_config_setting.py
+++ purchase_order_fob_inhancement/models/res_config_setting.py
@@ -6,10 +6,24 @@
 #
 ##############################################################################
 
-from odoo import fields, models
+from odoo import fields, models,api
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    fob = fields.Many2many('purchase.fob',string="FOB")
+    fob_ids = fields.Many2many('purchase.fob',string="FOB")
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        fob_value = self.env['ir.config_parameter'].get_param('purchase.fob_ids')
+        if fob_value:
+            res['fob_ids'] = [(6, 0, eval(fob_value))]
+        return res
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].set_param(
+            'purchase.fob_ids', self.fob_ids.ids
+        )
