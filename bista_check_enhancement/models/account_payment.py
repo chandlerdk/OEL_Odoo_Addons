09d1532fe6581ed640b8f1f1747bf9a92c48db78
Sachin Shelke <sachin.shelke@bista-it-00274.bistasolutions.com>
2024-10-22 15:57:50 +0530

    [ADD] vendor credit note details on check

--- bista_check_enhancement/models/account_payment.py
+++ bista_check_enhancement/models/account_payment.py
@@ -62,7 +62,7 @@ class AccountPayment(models.Model):
         """
         self.ensure_one()
 
-        def prepare_vals(invoice, partials):
+        def prepare_vals(invoice, partials,account_moves):
             number = invoice.ref if invoice.ref else ''
             if invoice.is_outbound() or invoice.move_type == 'in_receipt':
                 invoice_sign = 1
@@ -81,6 +81,7 @@ class AccountPayment(models.Model):
             return {
                 'due_date': format_date(self.env, invoice.invoice_date),
                 'date':format_date(self.env, invoice.date),
+                'account_moves': account_moves,
                 'name':invoice.name,
                 'ref':invoice.ref,
                 'number': number,
@@ -95,9 +96,12 @@ class AccountPayment(models.Model):
         # Decode the reconciliation to keep only invoices.
         term_lines = self.line_ids.filtered(
             lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable'))
+
         invoices = (
                     term_lines.matched_debit_ids.debit_move_id.move_id + term_lines.matched_credit_ids.credit_move_id.move_id) \
             .filtered(lambda x: x.is_outbound() or x.move_type == 'in_receipt')
+
+
         invoices = invoices.sorted(lambda x: x.invoice_date_due or x.date)
 
         # Group partials by invoices.
@@ -111,18 +115,27 @@ class AccountPayment(models.Model):
             if invoice in invoice_map:
                 invoice_map[invoice] |= partial
 
+        reconciled_lines = self.move_id.line_ids._all_reconciled_lines().filtered(
+            lambda l: (l.matched_debit_ids or l.matched_credit_ids) and l.debit > 0
+        )
+        account_move_lines = reconciled_lines.filtered(lambda l: l.move_id.move_type != 'entry')
+        account_move_ids = self.env['account.move.line'].browse(account_move_lines.ids)
+        move_id = self.env['account.move']
+        for line in account_move_ids:
+            move_id |= line.move_id
+
         # Prepare stub_lines.
         if 'out_refund' in invoices.mapped('move_type'):
             stub_lines = [{'header': True, 'name': "Bills"}]
-            stub_lines += [prepare_vals(invoice, partials)
+            stub_lines += [prepare_vals(invoice, partials, move_id)
                            for invoice, partials in invoice_map.items()
                            if invoice.move_type == 'in_invoice']
             stub_lines += [{'header': True, 'name': "Refunds"}]
-            stub_lines += [prepare_vals(invoice, partials)
+            stub_lines += [prepare_vals(invoice, partials,move_id)
                            for invoice, partials in invoice_map.items()
                            if invoice.move_type == 'out_refund']
         else:
-            stub_lines = [prepare_vals(invoice, partials)
+            stub_lines = [prepare_vals(invoice, partials,move_id)
                           for invoice, partials in invoice_map.items()
                           if invoice.move_type in ('in_invoice', 'in_receipt')]
 
