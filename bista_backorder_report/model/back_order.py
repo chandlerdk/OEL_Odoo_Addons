55eeae62972256ad0e341694aeeb9c9fe913bb2f
Punit Chaudhary <punit.chaudhary@bista-it-00220.bistasolutions.com>
2024-10-30 18:07:02 +0530

    [ADD]Imp in code for invoice report and Back order report

--- bista_backorder_report/model/back_order.py
+++ bista_backorder_report/model/back_order.py
@@ -15,27 +15,24 @@ class NegativeForecast(models.Model):
     sale_id = fields.Many2one('sale.order',string='Sale Order')
     qty = fields.Float(string="Quantity")
     sale_price =fields.Float('Sale Price')
+    total_amount = fields.Float('Amount Total')
 
     def back_order_report_generation(self):
-        # 1. Fetch products with currently negative stock (qty_available < 0)
         outgoing_type_id = self.env['stock.picking.type'].search([('code', '=', 'outgoing')], limit=1)
-
         moves = self.env['stock.move'].search(
-            [('state','=','assigned'),('picking_type_id', '=', outgoing_type_id.id)]  # Filter for current negative quantities
+            [('state','=','assigned'),('picking_type_id', '=', outgoing_type_id.id)]
         )
 
         for move in moves:
-
             sale_id = self.env['sale.order'].search([('name','=',move.origin)])
-
             self.env['back.order.report'].create({
                 'product_id': move.product_id.id,
                 'partner_id': move.partner_id.id,
                 'sale_id': sale_id.id,
                 'qty': move.product_uom_qty ,
                 'sale_price': move.product_id.lst_price,
+                'total_amount':move.product_uom_qty * move.product_id.lst_price,
             })
-
 
     def refresh_back_order_report(self):
         query = "DELETE FROM back_order_report"
@@ -46,10 +43,3 @@ class NegativeForecast(models.Model):
         query = "DELETE FROM back_order_report"
         self.env.cr.execute(query)
         self.back_order_report_generation()
-
-
-class StockMove(models.Model):
-    _inherit = 'stock.move'
-    _description = 'Stock Move'
-
-    flag = fields.Boolean('')
