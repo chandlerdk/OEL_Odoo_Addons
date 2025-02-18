0f0e156cf3cbeb3172dba4bae811400bd9cbec83
Sachin Shelke <sachin.shelke@bista-it-00274.bistasolutions.com>
2025-02-18 12:18:02 +0530

    [ADD] added early payment discount in discount applied

--- bista_delivery_slip/models/account_move.py
+++ bista_delivery_slip/models/account_move.py
@@ -8,6 +8,11 @@ class AccountMove(models.Model):
         compute="_compute_discount_total",
         currency_field='currency_id'
     )
+    payment_difference_amount = fields.Float(
+        string="Payment Difference",
+        help="Stores the payment difference from the payment wizard",
+        copy=False
+    )
 
     @api.depends('invoice_line_ids.discount', 'invoice_line_ids.price_unit', 'invoice_line_ids.quantity')
     def _compute_discount_total(self):
@@ -16,4 +21,17 @@ class AccountMove(models.Model):
                 line.price_unit * line.quantity * (line.discount / 100)
                 for line in move.invoice_line_ids if line.discount
             )
-            move.discount_total = total_discount
+            move.discount_total = total_discount + move.payment_difference_amount
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+    
+
+    def _reconcile_payments(self, to_process, edit_mode=False):
+        res = super()._reconcile_payments(to_process, edit_mode=False)
+        if self.line_ids:
+            for line in self.line_ids:
+                move = self.env['account.move'].search([('name', '=', line.name)], limit=1)
+                if move and self.early_payment_discount_mode:
+                    move.write({'payment_difference_amount': self.payment_difference})
+        return res
