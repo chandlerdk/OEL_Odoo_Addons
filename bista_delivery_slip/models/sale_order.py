c29bb65ee5e5dc5d28778c0d266197a8d284f5d4
Sachin Shelke <sachin.shelke@bista-it-00274.bistasolutions.com>
2025-02-25 16:02:44 +0530

    [ADD] added sale order total dicount on order level

---
+++ bista_delivery_slip/models/sale_order.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import models, fields, api
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    discount_total = fields.Monetary(
+        string="Discount Applied",
+        compute="_compute_discount_total",
+        currency_field='currency_id'
+    )
+
+    @api.depends('order_line.discount', 'order_line.price_unit', 'order_line.product_uom_qty')
+    def _compute_discount_total(self):
+        for order in self:
+            total_discount = sum(
+                line.price_unit * line.product_uom_qty * (line.discount / 100)
+                for line in order.order_line if line.discount
+            )
+            order.discount_total = total_discount
