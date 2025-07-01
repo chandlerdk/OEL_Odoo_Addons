1869dcb5e56b063d836290f0bc53fcb0afdc96db
chandlerdk <chandler.duke@oelsales.com>
2025-07-01 13:36:54 -0600

    [IMP]  Only block real team_id changes in sale.order.write

--- OEL_sales_team_restriction/models/sale_order.py
+++ OEL_sales_team_restriction/models/sale_order.py
@@ -1,8 +1,5 @@
-# -- coding: utf-8 --
-#
-#########################################
-
-from odoo import models, api 
+# -*- coding: utf-8 -*-
+from odoo import models, api
 from odoo.exceptions import AccessError
 
 class SaleOrder(models.Model):
@@ -10,11 +7,24 @@ class SaleOrder(models.Model):
 
     @api.model
     def create(self, vals):
-        if 'team_id' in vals and not self.env.user.has_group('OEL_sales_team_restriction.group_sale_team_manager'):
+        # if they try to set a team on create and aren't in the manager group, block
+        team_id = vals.get('team_id')
+        if team_id and not self.env.user.has_group(
+            'OEL_sales_team_restriction.group_sale_team_manager'
+        ):
             raise AccessError("You are not allowed to set the Sales Team.")
-        return super(SaleOrder, self).create(vals)
+        return super().create(vals)
 
     def write(self, vals):
-        if 'team_id' in vals and not self.env.user.has_group('OEL_sales_team_restriction.group_sale_team_manager'):
-            raise AccessError("You are not allowed to change the Sales Team.")
-        return super(SaleOrder, self).write(vals)
+        # only enforce on actual team changes
+        if 'team_id' in vals:
+            new_id = vals.get('team_id')
+            # skip if nobody is really changing it
+            for order in self:
+                if order.team_id.id != new_id:
+                    if not self.env.user.has_group(
+                        'OEL_sales_team_restriction.group_sale_team_manager'
+                    ):
+                        raise AccessError("You are not allowed to change the Sales Team.")
+        return super().write(vals)
+
