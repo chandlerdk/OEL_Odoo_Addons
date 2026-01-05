789a0724c9283a592766a7baf436f49d3d488e7f
chandlerdk <root@ip-10-28-8-142.us-east-2.compute.internal>
2026-01-05 00:06:19 -0700

    [ADD] Email templates for forrest

---
+++ OEL_custom_forrest_templates/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "OEL Forrest Templates",
+    "summary": "Email templates for quotation follow up, OEL accounting information, and introductions",
+    "version": "17.0.1.0.0",
+    "author": "OEL",
+    "website": "https://www.oelsales.com",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "depends": ["sale_management", "account", "portal", "mail"],
+    "data": [
+        "data/mail_template_sale_quote_followup_oel.xml",
+        "data/mail_template_oel_accounting_info.xml",
+        "data/mail_template_oel_training_intro.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
