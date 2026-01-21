1dfaef0f8178aee1c94df89f9cf42f783e9e5cc4
chandlerdk <chandler.duke@oelsales.com>
2026-01-21 12:34:38 -0700

    Delete payment_ebizcharge_crm directory

--- payment_ebizcharge_crm/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import models
-from . import controllers
-from . import wizard
-from . import reports
-from . import utils
-# from . import test
