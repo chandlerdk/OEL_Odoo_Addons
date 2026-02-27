c154e4995274e938c73a57e09baaa6f539c7c503
chandlerdk <chandler.duke@oelsales.com>
2026-02-27 09:22:13 -0700

    Delete inv_stat.py

--- inv_stat.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    gln = fields.Char(string='GLN', required=False,readonly=False)
-    dc = fields.Many2one(string='Distribution Center', comodel_name='res.partner', required=False)
-    tp_id = fields.Many2one(string='Trading Partner', comodel_name='bsiedi.tradingpartner', required=False)
