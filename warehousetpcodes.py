ce0092182cac7570ef15fa2247b9e69732f16005
chandlerdk <chandler.duke@oelsales.com>
2026-02-27 08:57:48 -0700

    Delete warehousetpcodes.py

--- warehousetpcodes.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-class DeliveryCarrier(models.Model):
-    _inherit = 'delivery.carrier'
-
-    scac = fields.Char(string='SCAC', required=False,readonly=False)
-    carrier_account = fields.Char(string='Carrier Account', required=False,readonly=False)
