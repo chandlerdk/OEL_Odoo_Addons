9979f530bd136337e2e05ae169692a4ecf0ff6e0
chandlerdk <chandler.duke@oelsales.com>
2026-02-27 09:24:15 -0700

    Delete res_partner.py

--- res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-class StockLocation(models.Model):
-    _inherit = 'stock.location'
-
-    water_location = fields.Boolean(string='On the water?', required=False,readonly=False)
-    water_delay = fields.Integer(string='Expected days on the water',default=1, required=False)
