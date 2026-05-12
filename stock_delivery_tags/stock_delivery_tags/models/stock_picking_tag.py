34b5072a19b09a1b37d89c3105aa568198533df3
chandlerdk <chandler.duke@oelsales.com>
2026-05-12 14:37:41 -0600

    Delete stock_delivery_tags directory

--- stock_delivery_tags/stock_delivery_tags/models/stock_picking_tag.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class StockPickingTag(models.Model):
-    _name = 'stock.picking.tag'
-    _description = 'Stock Picking Tag'
-
-    name = fields.Char(string='Tag Name', required=True)
-    color = fields.Integer(string='Color Index', default=0)
-    active = fields.Boolean(default=True)
