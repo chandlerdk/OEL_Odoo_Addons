b8c577cd8bc87943a907bc909a77350cae6229fa
Sachin Shelke <sachin.shelke@bista-it-00348.bistasolutions.com>
2026-09-11 12:56:00 +0530

    [ADD]purchase_order_fob_inhancement;added fob configuration menus and most relatevant fob numbers can be add the favorite numberes

--- purchase_order_fob_inhancement/models/purchase_fob.py
+++ purchase_order_fob_inhancement/models/purchase_fob.py
@@ -1,10 +1,40 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class PurchaseFob(models.Model):
     _name = 'purchase.fob'
+    _description = 'Purchase FOB Number'
+    _order = 'is_favorite desc, sequence, name, id'
 
-    name = fields.Char(string="Name")
+    name = fields.Char(string='FOB Number', required=True, index=True)
+    sequence = fields.Integer(string='Sequence', default=10)
+    active = fields.Boolean(string='Active', default=True)
+    is_favorite = fields.Boolean(
+        string='Favorite',
+        default=False,
+        help='Favorite FOB numbers appear at the top of the list on purchase orders.',
+    )
+    purchase_order_count = fields.Integer(
+        string='Purchase Orders',
+        compute='_compute_purchase_order_count',
+    )
+
+
+    def _compute_purchase_order_count(self):
+        PurchaseOrder = self.env['purchase.order']
+        for fob in self:
+            fob.purchase_order_count = PurchaseOrder.search_count([('fob', '=', fob.id)])
+
+    def action_view_purchase_orders(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Purchase Orders',
+            'res_model': 'purchase.order',
+            'view_mode': 'tree,form',
+            'domain': [('fob', '=', self.id)],
+            'context': {'default_fob': self.id},
+        }
--- purchase_order_fob_inhancement/models/purchase_order.py
+++ purchase_order_fob_inhancement/models/purchase_order.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, fields
+from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-
-    ship_via = fields.Char(string="Ship Via")
-    fob = fields.Many2one('purchase.fob',string="FOB",)
-
+    ship_via = fields.Char(string='Ship Via')
+    fob = fields.Many2one(
+        'purchase.fob',
+        string='FOB',
+        help='FOB numbers are managed under Purchase > Configuration > FOB Numbers. '
+             'Favorites and lower sequence values appear first.',
+    )
